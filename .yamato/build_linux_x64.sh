e1a356f755b684dfeef515a832596204e7f29543
ashwini <ashwini@unity3d.com>
2019-04-10 16:13:14 -0400

    Run as sudo

--- .yamato/build_linux_x64.sh
+++ .yamato/build_linux_x64.sh
@@ -3,6 +3,6 @@ git submodule update --init --recursive
 cd external/buildscripts
 ./bee
 cd ../..
-perl external/buildscripts/build_runtime_linux.pl -build64=1 --stevedorebuilddeps=1
+sudo perl external/buildscripts/build_runtime_linux.pl -build64=1 --stevedorebuilddeps=1
 mkdir -p incomingbuilds/linux64
 cp -r builds/* incomingbuilds/linux64/
